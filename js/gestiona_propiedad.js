03ab9a9de64d4f0399dd6b373b21c548a05c30ed
luciarosales <luciarosales@uma.es>
2024-05-28 18:22:42 +0200

    Arreglo envio de mail  y accesibilidad

--- js/gestiona_propiedad.js
+++ js/gestiona_propiedad.js
@@ -41,16 +41,35 @@ document.addEventListener('DOMContentLoaded', function() {
             firstAttempt = false;
             validateForm();
         } else {
+            event.preventDefault(); // Prevenir el comportamiento de envío por defecto
+
+            // Obtiene los valores de los campos del formulario
+            let nombre = document.querySelector('input[type="text"]').value;
+            let telefono = document.querySelector('input[type="tel"]').value;
+            let correo = document.querySelector('input[type="email"]').value;
+            let mensaje = document.querySelector('textarea').value;
+
+            // Construye el contenido del correo
+            let contenido = `Nombre: ${nombre}\nTeléfono: ${telefono}\nCorreo: ${correo}\nMensaje: ${mensaje}`;
+
+            // Abre el cliente de correo con los datos predefinidos
+            window.open(`mailto:iumalagadreamhome@gmail.com?subject=Consulta&body=${encodeURIComponent(contenido)}`);
+
             form.classList.add('was-validated');
         }
     }, false);
 });
 
-
 function showHelpMessage() {
-   
     // Mostrar el mensaje de ayuda
     alert("Todos los campos con el símbolo '*' son obligatorios");
 
-}
+    // Crear un elemento de audio
+    var audioElement = document.createElement('audio');
 
+    // Establecer la ruta del archivo de audio
+    audioElement.setAttribute('src', 'sonidos/Sonido_ventana.mp3');
+
+    // Reproducir el sonido
+    audioElement.play();
+}
